e1f7406d04ad2f396a05c4dffcfee2037f28df9d
Kedar Vaidya <65109826+Kedar-V@users.noreply.github.com>
2023-03-07 15:57:54 +0530

    Update DatabaseVerticle.java

--- src/main/java/com/example/vertx_workshop/DatabaseVerticle.java
+++ src/main/java/com/example/vertx_workshop/DatabaseVerticle.java
@@ -1,23 +1,135 @@
 package com.example.vertx_workshop;
 
+import config.AppConfigStore;
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
 import io.vertx.core.Promise;
+import io.vertx.core.impl.logging.Logger;
+import io.vertx.core.impl.logging.LoggerFactory;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
+import io.vertx.jdbcclient.JDBCConnectOptions;
+import io.vertx.jdbcclient.JDBCPool;
+import io.vertx.sqlclient.PoolOptions;
+import io.vertx.sqlclient.Row;
+import io.vertx.sqlclient.RowSet;
+import io.vertx.sqlclient.Tuple;
 
 public class DatabaseVerticle extends AbstractVerticle {
 
+  private static final Logger LOGGER = LoggerFactory.getLogger(DatabaseVerticle.class);
+
+  JDBCPool pool;
+
   @Override
-  public void start(Promise<Void> startPromise) throws Exception {
-    vertx.createHttpServer().requestHandler(req -> {
-      req.response()
-        .putHeader("content-type", "text/plain")
-        .end("Hello from Vert.x!");
-    }).listen(8888, http -> {
-      if (http.succeeded()) {
-        startPromise.complete();
-        System.out.println("HTTP server started on port 8888");
-      } else {
-        startPromise.fail(http.cause());
-      }
+  public void start(Promise<Void> start) throws Exception{
+
+    LOGGER.info("DatabaseVerticle Started");
+
+//    vertx.executeBlocking( promise -> {
+//
+//      // Create DB Pool
+//        pool = JDBCPool.pool(
+//                vertx,
+//                new JDBCConnectOptions()
+//                        .setJdbcUrl(AppConfigStore.getConfig().dbConfig.getJdbcUrl())
+//                        .setUser(AppConfigStore.getConfig().dbConfig.getUser())
+//                        .setPassword(AppConfigStore.getConfig().dbConfig.getPassword()),
+//                new PoolOptions()
+//                        .setMaxSize(AppConfigStore.getConfig().dbConfig.getMaxPoolSize())
+//        );
+//
+//        // Query
+//      pool.query("SELECT * FROM CUSTOMERS")
+//        .execute()
+//        .onFailure(e -> {
+//            LOGGER.error(e);
+//          }
+//        )
+//        .onSuccess(rows -> {
+//          for (Row row: rows){
+//            LOGGER.info(row.getString("NAME"));
+//          }
+//        });
+//
+//      promise.complete();
+//    }, asyncResult -> {
+//      if (asyncResult.failed()){
+//        start.fail(asyncResult.cause());
+//      }
+//      else{
+//        start.complete();
+//      }
+//    });
+
+    vertx.eventBus().consumer("getData.addr", msg -> {
+      databaseConnection().onSuccess( fut -> {
+        String q = "SELECT * FROM CUSTOMERS";
+        runSQLQuery(q).onFailure(e -> {LOGGER.error(e);})
+          .onSuccess(rows -> {
+            JsonArray array = new JsonArray();
+            LOGGER.info("SUCCESS");
+            for (Row row: rows){
+              array.add(new JsonObject()
+                .put("Name", row.getString("NAME"))
+                .put("Age", row.getInteger("AGE"))
+                .put("Address", row.getString("ADDRESS")));
+            }
+            LOGGER.info(array);
+            msg.reply(array.toString());
+          });
+      });
     });
+
+    vertx.eventBus().consumer("getUserData.addr", msg -> {
+      databaseConnection().onSuccess( fut -> {
+        String q = "SELECT * FROM CUSTOMERS WHERE NAME = ?";
+        String name = (String) msg.body();
+        runSQLQuery(q, name).onFailure(e -> {LOGGER.error(e);})
+          .onSuccess(rows -> {
+            JsonArray array = new JsonArray();
+            LOGGER.info("SUCCESS");
+            for (Row row: rows){
+              array.add(new JsonObject()
+                .put("Name", row.getString("NAME"))
+                .put("Age", row.getInteger("AGE"))
+                .put("Address", row.getString("ADDRESS")));
+            }
+            LOGGER.info(array);
+            msg.reply(array.toString());
+          });
+      });
+    });
+  }
+
+  private Future<Object> databaseConnection(){
+
+    Promise<Object> dbPromise = Promise.promise();
+    Future<Object> dbFuture = dbPromise.future();
+
+    vertx.executeBlocking(promise -> {
+      pool = JDBCPool.pool(
+        vertx,
+        new JDBCConnectOptions()
+          .setJdbcUrl(AppConfigStore.getConfig().dbConfig.getJdbcUrl())
+          .setUser(AppConfigStore.getConfig().dbConfig.getUser())
+          .setPassword(AppConfigStore.getConfig().dbConfig.getPassword()),
+        new PoolOptions()
+          .setMaxSize(AppConfigStore.getConfig().dbConfig.getMaxPoolSize())
+      );
+      promise.complete();
+    }, asyncResult -> { if (asyncResult.failed()) {dbPromise.fail(asyncResult.cause());} else {dbPromise.complete();} });
+
+    return dbFuture;
+  }
+
+  private Future<RowSet<Row>> runSQLQuery(String q, String name){
+    return pool.preparedQuery(q)
+      .execute(Tuple.of(name));
+  }
+
+  private Future<RowSet<Row>> runSQLQuery(String q){
+    return pool.query(q)
+      .execute();
   }
 }
