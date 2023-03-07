c02a7048cf29f8959af83259dd2a2a6e3093f6f8
Kedar Vaidya <65109826+Kedar-V@users.noreply.github.com>
2023-03-07 15:57:39 +0530

    Update HelloVerticle.java

--- src/main/java/com/example/vertx_workshop/HelloVerticle.java
+++ src/main/java/com/example/vertx_workshop/HelloVerticle.java
@@ -1,4 +1,29 @@
 package com.example.vertx_workshop;
 
-public class HelloVerticle {
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.impl.logging.Logger;
+import io.vertx.core.impl.logging.LoggerFactory;
+
+import java.util.UUID;
+
+public class HelloVerticle extends AbstractVerticle {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(HelloVerticle.class);
+
+    String verticleId = UUID.randomUUID().toString();
+
+    @Override
+    public void start() throws Exception{
+
+      LOGGER.info("HelloVerticle Started");
+
+      vertx.eventBus().consumer("hello.addr", msg -> {
+        LOGGER.info("here");
+        msg.reply("Hello Vert.x");
+      });
+
+      vertx.eventBus().consumer("route1.addr", msg -> {
+        msg.reply(String.format("Hello Vert.x %s, VerticleId: %s", (String) msg.body(), verticleId));
+      });
+    }
 }
