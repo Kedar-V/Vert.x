325a26d95beaa270d4bbfec910c62b18684751ee
Kedar Vaidya <65109826+Kedar-V@users.noreply.github.com>
2023-03-07 15:58:25 +0530

    Update Config.java

--- src/main/java/config/Config.java
+++ src/main/java/config/Config.java
@@ -1,4 +1,18 @@
 package config;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Config {
+
+  public VertxConfig vertx;
+
+  public VertxConfig getVertx() {
+    return vertx;
+  }
+
+  public DbConfig dbConfig;
+
+  public DbConfig getDbConfig() {return dbConfig;}
+
 }
