058c1e65f8c6004f03563a1a78832193187dfa7b
Kedar Vaidya <65109826+Kedar-V@users.noreply.github.com>
2023-03-07 15:58:11 +0530

    Update AppConfigStore.java

--- src/main/java/config/AppConfigStore.java
+++ src/main/java/config/AppConfigStore.java
@@ -1,4 +1,14 @@
 package config;
 
 public class AppConfigStore {
+
+  public static Config getConfig() {
+    return config;
+  }
+
+  public static void setConfig(Config config) {
+    AppConfigStore.config = config;
+  }
+
+  public static Config config;
 }
