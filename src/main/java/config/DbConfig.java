a90a86b815546ad455327db08915e130e92f700a
Kedar Vaidya <65109826+Kedar-V@users.noreply.github.com>
2023-03-07 15:58:42 +0530

    Update DbConfig.java

--- src/main/java/config/DbConfig.java
+++ src/main/java/config/DbConfig.java
@@ -1,4 +1,47 @@
 package config;
 
+import com.fasterxml.jackson.annotation.JsonTypeName;
+
+@JsonTypeName("dbConfig")
 public class DbConfig {
+
+  private String jdbcUrl;
+  private String user;
+  private String password;
+
+  private Integer maxPoolSize;
+
+  public Integer getMaxPoolSize() {
+    return maxPoolSize;
+  }
+
+  public void setMaxPoolSize(Integer maxPoolSize) {
+    this.maxPoolSize = maxPoolSize;
+  }
+
+  public String getJdbcUrl() {
+    return jdbcUrl;
+  }
+
+  public void setJdbcUrl(String jdbcUrl) {
+    this.jdbcUrl = jdbcUrl;
+  }
+
+  public String getUser() {
+    return user;
+  }
+
+  public void setUser(String user) {
+    this.user = user;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+
 }
